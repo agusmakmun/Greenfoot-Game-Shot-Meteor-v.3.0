cf70082b89ed6ca62f53cd71602ce34691de87c4
agus makmun <summon.agus@gmail.com>
2015-04-02 23:33:33 +0700

    Create setPeluru.java

---
+++ Actor/setPeluru.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah setPeluru(); yang merupakan kumpulan dari peluru, dan sesuai dengan masing-masing Level.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.0.2)
+ */
+public class setPeluru extends Actor
+{
+    /**
+     * Act - do whatever the setPeluru wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
