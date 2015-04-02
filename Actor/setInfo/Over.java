a2dd53dc9dbf8f58d98e371bbb836ed7f73ccc88
agus makmun <summon.agus@gmail.com>
2015-04-02 23:48:36 +0700

    Create Over.java

---
+++ Actor/setInfo/Over.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah Over(); hanya sebatas image ketika game over.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+public class Over extends setInfo
+{
+    /**
+     * Act - do whatever the Over wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
