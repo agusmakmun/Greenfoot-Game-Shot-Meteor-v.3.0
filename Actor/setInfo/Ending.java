db16bd2d5c52c87f0dc396fedb75b378851af2e0
agus makmun <summon.agus@gmail.com>
2015-04-02 23:49:22 +0700

    Create Ending.java

---
+++ Actor/setInfo/Ending.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah Ending(); yang merupakan informasi yang berupa image dengan tulisan -->
+ * "Congratulations", dan ini merupakan turunan dari setInfo();
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+
+public class Ending extends setInfo
+{
+    /**
+     * Act - do whatever the Ending wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
