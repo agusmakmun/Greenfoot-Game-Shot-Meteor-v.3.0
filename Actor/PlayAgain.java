dd175586577d8f9ada6d5185ee853042c279eef6
agus makmun <summon.agus@gmail.com>
2015-04-02 23:40:59 +0700

    Create PlayAgain.java

---
+++ Actor/PlayAgain.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah button PlayAgain(); yang digunakan sebagai button play starter -->
+ * yang ketika di klik, akan menuju ke Level1();
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.2.0)
+ */
+public class PlayAgain extends Actor
+{
+    public void act() 
+    {
+        if (Greenfoot.mousePressed(this) )  
+        {   
+            Greenfoot.setWorld(new Level1()); //press button masuk ke Level1();
+        }
+    }    
+}
