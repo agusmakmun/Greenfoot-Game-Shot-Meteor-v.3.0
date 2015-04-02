356938ba6e81207d3c3a88542c84a195267df522
agus makmun <summon.agus@gmail.com>
2015-04-02 23:47:57 +0700

    Create Beginning.java

---
+++ Actor/setInfo/Beginning.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah Beginning(); yang merupakan informasi yang berupa image dengan tulisan -->
+ * "Game Shot Meteor", dan ini merupakan turunan dari setInfo();
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+public class Beginning extends setInfo
+{
+    /**
+     * Act - do whatever the Beginning wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
