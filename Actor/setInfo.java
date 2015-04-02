c9cbecad638306f1c90a13b4490514dc4d28b2aa
agus makmun <summon.agus@gmail.com>
2015-04-02 23:47:21 +0700

    Create setInfo.java

---
+++ Actor/setInfo.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah setInfo(); dimana didalamnya terdapat beberapa image informasi.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+
+public class setInfo extends Actor
+{
+    /**
+     * Act - do whatever the setInfo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
