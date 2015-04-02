f54696c59e810b446bdfa02a4662edf37121d773
agus makmun <summon.agus@gmail.com>
2015-04-02 23:41:27 +0700

    Create setCounter.java

---
+++ Actor/setCounter.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah setCounter(); dimana di sub class nya terdapat beberapa Counter -->
+ * yang sesuai dengan masing-masing level.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.2.0)
+ */
+
+public class setCounter extends Actor
+{
+    /**
+     * Act - do whatever the setCounter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
