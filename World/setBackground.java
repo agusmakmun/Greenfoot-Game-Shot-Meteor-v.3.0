90168cd8a3d378be41c4cb6e7a2c1958c4c63dd2
agus makmun <summon.agus@gmail.com>
2015-04-02 23:22:03 +0700

    Create setBackground.java

---
+++ World/setBackground.java
@@ -0,0 +1,16 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah primary setBackground untuk menaungi semua background
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+public class setBackground extends World
+{
+    public setBackground()
+    {    
+        super(800, 500, 1); //setting primary for all background
+    }
+}
