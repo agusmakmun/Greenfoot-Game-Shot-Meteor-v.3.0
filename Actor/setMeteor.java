00ddea14bc2520e8a52134852d9ec8cb05f82049
agus makmun <summon.agus@gmail.com>
2015-04-02 23:45:03 +0700

    Create setMeteor.java

---
+++ Actor/setMeteor.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah setMeteor(); yang merupakan kumpulan dari meteor, dan sesuai dengan masing-masing Level.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.2.0)
+ */
+public class setMeteor extends Actor
+{
+    /**
+     * Act - do whatever the setMeteor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
