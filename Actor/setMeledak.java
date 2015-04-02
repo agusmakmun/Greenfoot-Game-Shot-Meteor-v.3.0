784a78504c0840b073d4f2c6b89da563915bb38c
agus makmun <summon.agus@gmail.com>
2015-04-02 23:31:54 +0700

    Create setMeledak.java

---
+++ Actor/setMeledak.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah setMeledak(); yang merupakan kumpulan dari reaksi ledakan.
+ * 
+ * @author (Summon Agus - Agus Makmun - L200130113) 
+ * @blog (bloggersmart.net)
+ * @version (Update: v.3.0)
+ */
+public class setMeledak extends Actor
+{
+    /**
+     * Act - do whatever the setMeledak wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
